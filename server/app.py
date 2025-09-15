640ef27905f7c5c4b9b968a1982977444149b664
Okaroni Purity <nekesapurity20@gmail.com>
2025-09-16 01:02:50 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        message_list = Message.query.all()
+        return jsonify([m.to_dict() for m in message_list])
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict())
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = db.session.get(Message, id)
+    if request.method == 'GET':
+        if not message:
+            return make_response(jsonify({"error": "Message not found"}), 404)
+        return jsonify(message.to_dict())
+    elif request.method == 'PATCH':
+        if not message:
+            return make_response(jsonify({"error": "Message not found"}), 404)
+        data = request.get_json()
+        message.body = data['body']
+        db.session.commit()
+        return jsonify(message.to_dict())
+    elif request.method == 'DELETE':
+        if not message:
+            return make_response(jsonify({"error": "Message not found"}), 404)
+        db.session.delete(message)
+        db.session.commit()
+        return make_response('', 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
